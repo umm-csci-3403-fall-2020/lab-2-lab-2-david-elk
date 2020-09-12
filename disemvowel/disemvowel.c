5de5841a57db88d017145cd51cf4e56c158a4674
oswoo003 <oswoo003@umn.edu>
2020-09-12 16:36:56 -0500

    First attempt at disemvowel, non-functional

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -3,5 +3,30 @@
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+  //init stuff
+  char *result = (char*) calloc(count_vowels(str) + 1, sizeof(char));
+  char vowel = ('A'||'a'||'E'||'e'||'I'||'i'||'O'||'o'||'U'||'u'); //idk if this will work
+
+  //looping over the string
+  int i;
+  for(i = 0; str[i] != '\0'; i++) {
+    if (str[i] != vowel) {
+      result[i] = str[i];
+    }
+  }
+
+  return (char*) result;
+}
+
+//counts non-vowels in string for mem alloc
+int count_vowels(char *str) {
+  int result;
+  int i;
+  char vowel = ('A'||'a'||'E'||'e'||'I'||'i'||'O'||'o'||'U'||'u');
+  for(i = 0; str[i] != '\0'; i++) {
+    if (str[i] != vowel) {
+      result++;
+    }
+  }
+ return result;
 }
--- disemvowel/disemvowel.h
+++ disemvowel/disemvowel.h
@@ -2,5 +2,6 @@
 #define DISEMVOWEL_H_GUARD
 
 char *disemvowel(char *str);
+int count_vowels(char *str);
 
 #endif
